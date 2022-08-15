e870619d4b9c220b075f514cfdb4f8e830164de6
Ieuan Walker <ieuan.walker007@gmail.com>
2022-08-15 09:09:48 +0100

    update

--- Demo/DemoApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace DemoApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
